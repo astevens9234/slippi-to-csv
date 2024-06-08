3428fef83f844f97de0a9a2cc293892a2d927ee2
Adam <astevens9234@outlook.com>
2024-06-08 08:43:05 -0700

    first pass, mapped frames to csv

--- slippi-to-svc.go
+++ slippi-to-svc.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"reflect"
+	"sort"
 	"strconv"
+	"strings"
 
 	slippi "github.com/pmcca/go-slippi"
 )
+
+/*	- Work through csv mapping
+	- Read all slp from /slippi
+	- Write all csv to /output
+	- Concurrency
+*/
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -20,99 +28,151 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(game.Data.GameStart.GameMode)
+	gameToJSON(game)
+	gameToCSV(game)
 
-	// Write the data to a CSV file
-	if err := writeCsv("test.csv", game); err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("CSV file written successfully!")
-	}
 }
 
-func gameToSlice(v interface{}) []string {
-	var result []string
-	val := reflect.ValueOf(v)
-
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		switch field.Kind() {
-		case reflect.String:
-			result = append(result, field.String())
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			result = append(result, strconv.FormatInt(field.Int(), 10))
-		case reflect.Float32, reflect.Float64:
-			result = append(result, strconv.FormatFloat(field.Float(), 'f', -1, 64))
-		case reflect.Bool:
-			result = append(result, strconv.FormatBool(field.Bool()))
-		case reflect.Struct:
-			result = append(result, gameToSlice(field.Interface())...)
-		case reflect.Slice:
-			for j := 0; j < field.Len(); j++ {
-				result = append(result, fmt.Sprint(field.Index(j)))
-			}
-		default:
-			result = append(result, fmt.Sprint(field))
-		}
-	}
-	return result
-}
-
-func writeCsv(filename string, data interface{}) error {
-	file, err := os.Open(filename)
+// Write .slp to .json
+func gameToJSON(g interface{}) {
+	jd, err := json.MarshalIndent(g, "", "    ")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error marshaling to JSON: %v", err)
+	}
+
+	file, err := os.Create("test.json")
+	if err != nil {
+		log.Fatalf("Error creating file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(jd)
+	if err != nil {
+		log.Fatalf("Error writing JSON to file: %v", err)
+	}
+}
+
+// Write .slp to .csv -- WIP
+func gameToCSV(g interface{}) error {
+	file, err := os.Create("frames.csv")
+	if err != nil {
+		panic(err)
 	}
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	val := reflect.ValueOf(data)
-	if val.Kind() != reflect.Slice {
-		log.Fatal(val.Kind())
+	jd, err := json.MarshalIndent(g, "", "    ")
+	if err != nil {
+		log.Fatalf("Error marshaling to JSON: %v", err)
 	}
 
-	// Assuming all structs have the same fields and structure, use the first struct for the header
-	if val.Len() > 0 {
-		firstElement := val.Index(0)
-		header := getHeader(firstElement.Interface())
-		if err := writer.Write(header); err != nil {
-			return err
+	var d map[string]interface{}
+	if err := json.Unmarshal(jd, &d); err != nil {
+		log.Fatalf("Error unmarshaling JSON: %v", err)
+	}
+
+	data := d["Data"].(map[string]interface{})
+	frames := data["Frames"].(map[string]interface{})
+
+	// Get All Frame Nums
+	frameMap := []int{}
+	for key := range frames {
+		i, _ := strconv.Atoi(key)
+		frameMap = append(frameMap, i)
+	}
+	sort.Ints(frameMap)
+
+	frameNumber := "0"
+	sample := frames[frameNumber].(map[string]interface{})
+	flatMap := make(map[string]string)
+	flattenMap(sample, "", flatMap)
+
+	keys, err := sortedNumericKeys(flatMap)
+	if err != nil {
+		return err
+	}
+
+	k := append([]string{"Index"}, keys...)
+	if err := writer.Write(k); err != nil {
+		return err
+	}
+
+	// Everything to here is keep
+
+	// This is iterating beyond the elements of the list (by one) ...
+	for _, j := range frameMap {
+		frameNumber = strconv.Itoa(j)
+
+		frame := frames[frameNumber].(map[string]interface{})
+		flatMap := make(map[string]string)
+		flattenMap(frame, "", flatMap)
+
+		row := []string{}
+		row = append([]string{frameNumber}, row...)
+		for _, j := range keys {
+			row = append(row, flatMap[j])
 		}
-	}
 
-	// Write the data
-	for i := 0; i < val.Len(); i++ {
-		row := gameToSlice(val.Index(i).Interface())
 		if err := writer.Write(row); err != nil {
 			return err
 		}
 	}
 
 	return nil
+
 }
 
-// Helper function to get the header from a struct
-func getHeader(v interface{}) []string {
-	var header []string
-	val := reflect.ValueOf(v)
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+/*
+flattenMap is a recursive function that flattens a nested map into a flat map with composite keys.
+nestedMap: The
+*/
+func flattenMap(nestedMap map[string]interface{}, prefix string, flatMap map[string]string) {
+	for key, value := range nestedMap {
+		fullKey := key
+		if prefix != "" {
+			fullKey = prefix + "." + key
+		}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Type().Field(i)
-		if field.Type.Kind() == reflect.Struct {
-			nestedHeader := getHeader(val.Field(i).Interface())
-			header = append(header, nestedHeader...)
-		} else {
-			header = append(header, field.Name)
+		switch v := value.(type) {
+		case map[string]interface{}:
+			flattenMap(v, fullKey, flatMap)
+		case []interface{}:
+			for i, item := range v {
+				itemKey := fmt.Sprintf("%s.%d", fullKey, i)
+				if itemMap, ok := item.(map[string]interface{}); ok {
+					flattenMap(itemMap, itemKey, flatMap)
+				} else {
+					flatMap[itemKey] = fmt.Sprintf("%v", item)
+				}
+			}
+		default:
+			flatMap[fullKey] = fmt.Sprintf("%v", v)
 		}
 	}
-	return header
+}
+
+// TODO: This is not returning in the correct order: (-1, -10, -100, ... 0, 10, 100)
+func sortedNumericKeys(m map[string]string) ([]string, error) {
+	keys := make([]string, 0, len(m))
+	positiveKeys := []string{}
+	negativeKeys := []string{}
+
+	for key := range m {
+		if strings.HasPrefix(key, "-") {
+			negativeKeys = append(negativeKeys, key)
+		} else {
+			positiveKeys = append(positiveKeys, key)
+		}
+	}
+
+	sort.Strings(positiveKeys)
+	sort.Strings(negativeKeys)
+
+	// Concatenate negative keys followed by positive keys
+	keys = append(keys, negativeKeys...)
+	keys = append(keys, positiveKeys...)
+
+	return keys, nil
 }
